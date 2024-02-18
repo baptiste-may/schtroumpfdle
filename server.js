1c22d6a922448e5ddd037a0ac7dd1a1bb8a92d8c
DjRedstone <baptiste_may@ens.univ-artois.fr>
2024-02-18 23:28:43 +0100

    init

---
+++ server.js
@@ -0,0 +1,129 @@
+const mysql = require("mysql");
+const { PORT, MYSQL } = require("./config.json");
+const path = require("path");
+const express = require("express");
+const app = express();
+const HEADER_TYPE = {
+    id: -1,
+    name: 0,
+    sex: 0,
+    species: 0,
+    ennemies: 1,
+    looks: 1,
+    first_episode: 0,
+    img: -1
+};
+const cron = require("node-cron");
+let NB_FINDED = 0;
+let CURRENT_STMPF;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("src"));
+app.use(express.static("public"));
+
+const connection = mysql.createConnection(MYSQL);
+connection.connect(async err => {
+    if (err) throw err;
+    console.info("✅ Connected to database!");
+
+    const data = await query(connection, "SELECT COUNT(*) FROM Peoples;");
+    console.log(`${data[0]["COUNT(*)"]} schtroumpfs loaded!`);
+
+    await generateSchtroumpf();
+    cron.schedule("0 0 * * *", () => {
+        generateSchtroumpf(data);
+    }, {
+        timezone: "Europe/Paris"
+    });
+});
+
+
+async function generateSchtroumpf() {
+    const stmpf = (await query(connection, "SELECT * FROM Peoples ORDER BY RAND() LIMIT 1;"))[0];
+    parseStmpf(stmpf);
+    console.log(`Current schtroumpf : ${stmpf.name}`);
+    CURRENT_STMPF = stmpf;
+    NB_FINDED = 0;
+}
+
+function query(connection, sql) {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, (err, res) => {
+            if (err) reject(err);
+            resolve(res);
+        });
+    });
+}
+
+function parseStmpf(stmpf) {
+    for (const key in stmpf) {
+        if (typeof stmpf[key] == "string") {
+            if (stmpf[key].includes(",")) {
+                stmpf[key] = stmpf[key].split(",");
+            }
+        }
+    }
+}
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/index.html"))
+});
+
+app.get("/getSchtroumpfs", async (req, res) => {
+    const data = await query(connection, "SELECT * FROM Peoples;");
+    for (const stmpf of data) {
+        parseStmpf(stmpf);
+    }
+    res.send(data);
+});
+
+app.get("/testSchtroumpfs*", async (req, res) => {
+    if (!req.query.id) {
+        res.sendStatus(403);
+        return;
+    }
+    if (isNaN(parseInt(req.query.id))) {
+        res.sendStatus(403);
+        return;
+    }
+    const id = parseInt(req.query.id);
+    const testedStmpfs = await query(connection, `SELECT * FROM Peoples WHERE id = ${id};`);
+    if (testedStmpfs.length !== 1) {
+        res.sendStatus(404);
+        return;
+    }
+    const testedStmpf = testedStmpfs[0];
+    parseStmpf(testedStmpf);
+    const resJson = {};
+    for (const key in HEADER_TYPE) {
+        if (HEADER_TYPE[key] === 0) {
+            resJson[key] = testedStmpf[key] === CURRENT_STMPF[key];
+        } else if (HEADER_TYPE[key] === 1) {
+            if (typeof testedStmpf[key] === "string" && typeof CURRENT_STMPF[key] === "string") {
+                resJson[key] = testedStmpf[key] === CURRENT_STMPF[key];
+            } else if (typeof testedStmpf[key] === "string") {
+                resJson[key] = false;
+            } else {
+                resJson[key] = {};
+                for (const tag of testedStmpf[key]) {
+                    resJson[key][tag] = CURRENT_STMPF[key].includes(tag);
+                }
+            }
+        }
+    }
+    if (resJson.name && req.query.finded === "false") {
+        NB_FINDED ++;
+        console.log(`Schtroumpf finded! [${NB_FINDED}]`);
+    }
+    res.send(resJson);
+});
+
+app.get("/getNbFinded", (req, res) => {
+    res.send({nb: NB_FINDED});
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
---
+++ src/client.js
@@ -0,0 +1,177 @@
+const DEFAULT_INPUT_TEXT = "Schtroumpf ...";
+
+$(document).ready(() => {
+    $("#info-button").on("click", () => {
+        customAlert(`
+        <h1 class="ui header">Site web créé par <a href="https://www.linkedin.com/in/baptiste-may-8706602a3/">Baptiste May</a></h1>
+        <div class="ui icon teal message">
+            <i class="certificate icon"></i>
+            <div class="content">
+                <div class="header">Librairie CSS</div>
+                <p><a href="https://semantic-ui.com/">Semantic UI</a></p>
+            </div>
+        </div>
+        <div class="ui icon warning message">
+            <i class="exclamation triangle icon"></i>
+            <div class="content">
+                <div class="header">Vous avez rencontré un problème ?</div>
+                <p>N'hésitez pas à me contacter via <a href="mailto:pro@may-baptiste.fr">pro@may-baptiste.fr</a></p>
+            </div>
+        </div>
+        `);
+    });
+
+    const searchSmtpf = $("#search-smtpf");
+    const searchSubmit = $("#search-submit");
+
+    let lastProps = Cookies.get("last_props");
+    if (!lastProps) {
+        Cookies.set("last_props", `{"last_finded": 0, "last_updated": 0, "props": []}`);
+    }
+    lastProps = Cookies.get("last_props");
+    let { last_finded, last_updated, props } = JSON.parse(lastProps);
+    if (!sameDay(new Date(last_updated), new Date(getNowDate()))) {
+        updateCookies(last_finded, []);
+        props = [];
+    }
+
+    $.get(`/getSchtroumpfs`, async data => {
+        for (const d of data) {
+            searchSmtpf.append(`<option id="smtpf-${d.id}" value="${d.id}">${d.name} <img src="${d.img}"></option>`);
+        }
+        if (last_finded < getNowDate()) {
+            // Not finded
+            for (const id of props) {
+                await addProp(data[id]);
+            }
+            updateMessage(false);
+            $(".ui.dropdown").removeClass("disabled");
+            searchSubmit.removeClass("disabled");
+            searchSubmit.on("click", async () => {
+                searchSubmit.transition("pulse");
+                const input = $(".ui.dropdown .text");
+                input.addClass("default");
+                input.text(DEFAULT_INPUT_TEXT);
+                const id = parseInt(searchSmtpf.val());
+                const finded = await addProp(data[id], false);
+                props.push(id);
+                if (finded) {
+                    last_finded = getNowDate();
+                    $(".ui.dropdown").addClass("disabled");
+                    searchSubmit.addClass("disabled");
+                    updateMessage(true);
+                    customAlert(`<h1 class="ui header">
+                        <i class="check circle icon"></i>
+                        <div class="content">Bravo !</div>
+                        <div class="sub header">Vous avez trouvé le Schtroumpf !</div>
+                    </h1>
+                    <div class="ui blue card">
+                        <div class="image">
+                            <img src="${data[id].img}">
+                        </div>
+                        <div class="content">
+                            <div class="header">${data[id].name}</div>
+                            <div class="meta">${data[id].species}</div>
+                        </div>
+                        <div class="extra content">Première apparition : ${data[id].first_episode}</div>
+                    </div>`);
+                }
+                updateCookies(last_finded, props);
+            });
+            setInterval(() => {
+                searchSubmit.transition("tada");
+            }, 5000);
+        } else {
+            // Finded
+            for (const id of props) {
+                await addProp(data[id], true);
+            }
+            updateMessage(true);
+        }
+        // Finished loading
+        $("#container").removeClass("loading");
+    });
+
+    $.get("/getNbFinded", data => {
+        const nb = data.nb;
+        const message = (() => {
+            switch (nb) {
+                case 0:
+                    return "Personne n'a encore trouvé le Schtroumpf du jour !"
+                case 1:
+                    return "Une seule personne a trouvé le Schtroumpf du jour !"
+                default:
+                    return `${nb} personnes ont trouvé le Schtroumpf du jour !`
+            }
+        })();
+        const elt = $("#nb-fined");
+        elt.addClass("blue");
+        elt.html(`<i class="user icon"></i>` + message);
+    });
+});
+
+function request(url) {
+    return new Promise((resolve, reject) => {
+        $.get(url, data => {
+            resolve(data);
+        }).fail(err => {
+            reject(err);
+        });
+    });
+}
+
+function createSpans(smtpf, results) {
+    let res = "";
+    for (const key of smtpf) {
+        res += `<span class="ui ${results[key] ? 'green' : 'red'} label">${key}</span>`;
+    }
+    return res;
+}
+
+function getNowDate() {
+    return new Date(new Date().toDateString()).getTime();
+}
+
+function updateMessage(type) {
+    const message = $("#message");
+    if (type) {
+        message.removeClass("red");
+        message.addClass("green");
+        message.html("<i class='check circle icon'></i>Le Schtroumpf du jour a été trouvé !");
+    } else {
+        message.addClass("red");
+        message.html("<i class='exclamation circle icon'></i>Le Schtroumpf du jour n'a pas encore été trouvé.");
+    }
+}
+
+async function addProp(smtpf, finded) {
+    const id = smtpf.id;
+    const requestData = await request(`/testSchtroumpfs?id=${id}&finded=${finded}`);
+    $(`#smtpf-${id}`).remove();
+    $("#table-body").append(`<tr>
+        <td><img class="ui mini image" src="${smtpf.img}"></td>
+        <td class="${requestData.name ? 'positive' : 'negative'}">${smtpf.name}</td>
+        <td class="${requestData.sex ? 'positive' : 'negative'}">${smtpf.sex === "0" ? "Masculin" : "Feminin"}</td>
+        <td class="${requestData.species ? 'positive' : 'negative'}">${smtpf.species}</td>
+        <td class="${typeof smtpf.ennemies === "string" ? (requestData.ennemies ? "positive" : "negative") : ""}">${typeof smtpf.ennemies === "string" ? smtpf.ennemies : createSpans(smtpf.ennemies, requestData.ennemies)}</td>
+        <td>${createSpans(smtpf.looks, requestData.looks)}</td>
+        <td class="${requestData.first_episode ? 'positive' : 'negative'}">${smtpf.first_episode}</td>
+    </tr>`);
+    return requestData.name;
+}
+
+function updateCookies(last_finded, props) {
+    Cookies.set("last_props", JSON.stringify({last_finded: last_finded, last_updated: getNowDate(), props: props}));
+}
+
+function customAlert(html) {
+    $("#alert").transition("scale");
+    $("#alert-inside").html(html);
+}
+
+function sameDay(d1, d2) {
+    return d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate();
+}
+
